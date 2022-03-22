602a46ccfd773fa5cd7c48826e97bb96cf51d096
AlanGarc1a <alangarcia969@gmail.com>
2022-03-21 18:17:10 -0700

    add media queries and changed container properties

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-    max-width: 110rem;
-    margin: 0 auto;
-    padding-right: 3rem;
-    padding-left: 3rem;
+  max-width: 140rem;
+  margin: 0 auto;
+  padding-right: 3rem;
+  padding-left: 3rem;
+  padding-top: 6rem;
+
+  @media screen and (max-width: 768px) {
+    max-width: 76.8rem;
+  }
+
+  @media screen and (max-width: 480px) {
+    max-width: 48rem;
+  }
 `;
 
-export const Container: React.FC = ({children}) => {
-  return (
-    <Wrapper>
-        {children}
-    </Wrapper>
-  )
-}
+export const Container: React.FC = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
 
 export default Container;
