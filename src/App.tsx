2b7130bdeb80d8f2d1e0366825272f9d1df21421
AlanGarc1a <alangarcia969@gmail.com>
2022-03-23 18:25:27 -0700

    created route components and imported route pages for each route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SideBar from "./components/SideBar";
+import Navbar from "./components/NavBar/NavBar";
+import GlobalStyle from "./globalStyles";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/pages/Dashboard";
+import UpComing from "./components/pages/UpComing";
+import PopularMovies from "./components/pages/PopularMovies";
+import PopularTVShows from "./components/pages/PopularTVShows";
+import MovieView from "./components/pages/MovieView";
+import MovieSearch from "./components/pages/MovieSearch";
+import ShowSearch from "./components/pages/ShowSearch";
 
-function App() {
+const App: React.FC = () => {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const handleMenuClick = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Navbar open={openMenu} handleMenuClick={handleMenuClick} />
+      <SideBar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="upcoming-movies" element={<UpComing />} />
+        <Route path="popular-movies" element={<PopularMovies />} />
+        <Route path="top-tv-shows" element={<PopularTVShows />} />
+        <Route path=":movieTitle/:movieId" element={<MovieView />} />
+        <Route path="movie">
+          <Route path="search" element={<MovieSearch />} />
+        </Route>
+        <Route path="show">
+          <Route path="search" element={<ShowSearch />} />
+        </Route>
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
