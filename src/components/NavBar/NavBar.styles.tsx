407c3d667c026d1ee1e5978b4c76c177369f67da
AlanGarc1a <alangarcia969@gmail.com>
2022-03-29 18:40:49 -0700

    add navbar styles

--- src/components/NavBar/NavBar.styles.tsx
+++ src/components/NavBar/NavBar.styles.tsx
@@ -1,38 +1,93 @@
 import styled from "styled-components";
+import { BsSearch } from "react-icons/bs";
+import { FiChevronDown } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
-export const NavContainer = styled.aside`
-    position: fixed;
-    z-index: 1;
-    top: 0;
-    left: 0;
-    border-right: .1rem solid var(--secondary);
-    width: 12%;
-    height: 100%;
-    overflow-x: hidden;
+export const MenuContainer = styled.div`
+  display: none;
+  position: fixed;
+  width: 100%;
+  z-index: 99;
+  border-bottom: 0.1rem solid var(--white);
+
+  @media only screen and (max-width: 768px) {
+    display: block;
+  }
+
+  @media only screen and (max-width: 1024px) {
+    display: block;
+  }
 `;
 
-export const Nav = styled.nav`
-    display: flex;
-    flex-direction: column;
-    margin-left: 5rem;
-    padding-top: 5rem;
-    height: 100%;
+export const Menu = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 3rem;
+  background-color: var(--secondary);
 `;
 
-export const NavTitle = styled.h1`
-    color: var(--grey);
-    font-size: 2.8rem;
-    margin-bottom: 4rem;
+export const MenuItem = styled.div`
+  color: var(--white);
+  font-size: 2rem;
+  font-weight: 500;
+  text-transform: uppercase;
 `;
 
-export const NavItem = styled.div`
-    margin-bottom: 4rem;
+export const MenuContent = styled.div`
+  background-color: var(--primary);
+  border-radius: 1rem;
+  font-size: 1.5rem;
+  padding: 1.2rem 2.5rem;
+  margin-right: 2rem;
+  cursor: pointer;
 `;
 
-export const NavLink = styled.a`
-    display: inline-block;
-    width: 100%;
-    font-size: 1.8rem;
-    color: var(--grey);
-    cursor: pointer;
+export const MenuListContent = styled.div`
+  position: absolute;
+  right: 29%;
+  z-index: 99;
+  width: 35%;
+  margin-top: 1rem;
+  text-align: center;
+  border-radius: 0.2rem;
+  background-color: var(--white);
+`;
+
+export const MenuList = styled.ul`
+  list-style-type: none;
+  width: 100%;
+`;
+
+export const MenuListItem = styled.li`
+  font-size: 2rem;
+  width: 100%;
+  padding: 0.5rem;
+  text-transform: uppercase;
+
+  &:hover {
+    background-color: var(--light-grey);
+  }
+`;
+
+export const MenuListLink = styled(Link)`
+  display: inline-block;
+  color: var(--primary);
+  text-decoration: none;
+  width: 100%;
+`;
+
+export const MenuChevron = styled(FiChevronDown)`
+  font-size: 1.5rem;
+  margin-left: 1rem;
+`;
+
+export const MenuSearch = styled(BsSearch)`
+  font-size: 2.2rem;
+  color: var(--white);
+`;
+
+export const MenuHome = styled.span`
+  color: var(--white);
+  text-decoration: none;
 `;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Menu,
+  MenuItem,
+  MenuContainer,
+  MenuSearch,
+  MenuContent,
+  MenuChevron,
+  MenuListContent,
+  MenuList,
+  MenuListItem,
+  MenuListLink,
+  MenuHome,
+} from "./Navbar.styles";
+import { NavLinks } from "../SideBar";
+import { INavBarProps } from "../../models/navbarProps";
+import { Link } from "react-router-dom";
+
+const NavBar: React.FC<INavBarProps> = ({
+  open,
+  handleMenuClick,
+}: INavBarProps) => {
+  const renderMenuList = () => {
+    return NavLinks.map((link, index) => {
+      return (
+        <MenuListItem key={index}>
+          <MenuListLink to={link.url} onClick={handleMenuClick}>
+            {link.title}
+          </MenuListLink>
+        </MenuListItem>
+      );
+    });
+  };
+
+  return (
+    <>
+      <MenuContainer>
+        <Menu>
+          <MenuItem>
+            <Link to="/">
+              <MenuHome>MovieHub</MenuHome>
+            </Link>
+          </MenuItem>
+          <MenuItem>
+            <MenuContent onClick={handleMenuClick}>
+              Menu
+              <MenuChevron />
+            </MenuContent>
+            {open ? (
+              <></>
+            ) : (
+              <MenuListContent>
+                <MenuList>{renderMenuList()}</MenuList>
+              </MenuListContent>
+            )}
+          </MenuItem>
+          <MenuItem>
+            <Link to="/movie/search">
+              <MenuSearch />
+            </Link>
+          </MenuItem>
+        </Menu>
+      </MenuContainer>
+    </>
+  );
+};
+
+export default NavBar;
