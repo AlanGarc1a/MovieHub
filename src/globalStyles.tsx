f3f747d3ad6f313f825e6f9f082f77c6292c8176
AlanGarc1a <alangarcia969@gmail.com>
2022-03-29 18:44:06 -0700

    add html media query and new colors

--- src/globalStyles.tsx
+++ src/globalStyles.tsx
@@ -2,11 +2,17 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     :root {
-        --primary: #1A374D;
-        --secondary: #406882;
-        --dark-blue: #6998AB;
-        --grey: #eeeeee;
-        --dark: #041C32;
+        //colors
+        --primary: #2D3640;
+        --secondary: #8B9492;
+        --white: #F5F6F0;
+        --light-white: #DAE0D4;
+        --light-blue: #9DBABC;
+        --baby-blue: #ABCCD2;
+        --light-grey: #AFBABA;
+        --light-green: #23cf68;
+        --black: #07090C;
+
     }
 
     *,
@@ -17,15 +23,35 @@ const GlobalStyle = createGlobalStyle`
         outline: 0;
         box-sizing: inherit;
     }
-
+    
+    //default browser font-size: 16px
     html {
-        font-size: 62.5%;
+        font-size: 62.5%; //1rem = 10px 10px/16px=62.5%
     }
+    
+    @media only screen and (max-width: 1200px) { //1200px
+        html {
+            font-size: 50%; //1rem = 8px 8/16
+        }
+    }
+    
+    @media only screen and (max-width: 768px) { //768px
+        html {
+            font-size: 56%; //1rem=9px 9/16 = 56.25%
+        }
+    }
+    
+    @media only screen and (max-width: 480px) { //480px
+        html {
+            font-size: 48.75%; //1rem = 7.8px 7.8/16
+        }
+    }
+    
 
     body {
-        background-color: var(--primary);
+        background-color: var(--primary); 
         font-family: 'Roboto', 'Lato', 'Oswald', sans-serif;
-        font-family: 400;
+        font-weight: 400;
         box-sizing: border-box;
         line-height: 1.7;
     }
