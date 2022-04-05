ea95f27ca872ef617d63aa79082357be45c85c48
AlanGarc1a <alangarcia969@gmail.com>
2022-04-04 21:00:52 -0700

    change port

--- app.js
+++ app.js
@@ -6,18 +6,22 @@ const cors = require('cors');
 const path = require('path');
 const movieRoutes = require('./backend/routes/movieRoutes')
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 4000;
 
-const whitelist = ['http://localhost:8000', 'https://moviehub17.herokuapp.com']
+/* const whitelist = ['http://localhost:8000', 'https://moviehub17.herokuapp.com']
 const corsOptions = {
     origin: function (origin, callback) {
         if (!origin || whitelist.indexOf(origin) !== -1) callback(null, true)
         else callback(new Error('Not allowed by CORS'))
     },
     credentials: true,
-}
+} */
 
-app.use(cors(corsOptions));
+app.use(cors({
+    credentials: true,
+    origin: true
+}));
+app.options('*', cors());
 app.use(express.json());
 app.use('/api/movies', movieRoutes);
 
