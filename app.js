b93e8a37141a4f23e3cec8c2dfd4732488723aca
AlanGarc1a <alangarcia969@gmail.com>
2022-04-04 19:09:10 -0700

    add new build path

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const app = express();
 
 const port = 8000;
 
-app.use(express.static(path.join(__dirname, './build')));
-
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use('/api/movies', movieRoutes);
 
