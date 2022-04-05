7ee2723cabbe6f787f4dee4112183836d25e0a5c
AlanGarc1a <alangarcia969@gmail.com>
2022-04-04 19:35:07 -0700

    add node env for production to serve build files

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const path = require('path');
 const movieRoutes = require('./backend/routes/movieRoutes')
 const app = express();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'build')));
 
-
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('build'));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
 
 app.use('/api/movies', movieRoutes);
 
