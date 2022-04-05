7035066ab86faad142bc7b5160682bd49cdf0b07
AlanGarc1a <alangarcia969@gmail.com>
2022-04-04 20:20:23 -0700

    intercept each request and set response for cors

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const movieRoutes = require('./backend/routes/movieRoutes')
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.use(cors());
 app.use(express.json());
