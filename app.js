de35fe78c3bb9775444616c94d3036e60a81912f
AlanGarc1a <alangarcia969@gmail.com>
2022-04-04 20:25:19 -0700

    add heroku url to whitelist options

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const movieRoutes = require('./backend/routes/movieRoutes')
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+const whitelist = ['http://localhost:8000', 'https://moviehub17.herokuapp.com/']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) callback(null, true)
+        else callback(new Error('Not allowed by CORS'))
+    },
+    credentials: true,
+}
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/movies', movieRoutes);
 
